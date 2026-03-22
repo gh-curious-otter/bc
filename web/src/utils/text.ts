78059e9dbd3632a265f117bdfba510e2fee86289
gh-curious-otter <puneet@bc-infra.com>
2026-03-22 15:11:57 +0530

    fix(web): fix eslint errors and update Cron test (#2477)
    Fixes 3 no-control-regex lint errors in utils/text.ts. Updates Cron test to use regex matcher for text split across elements after CRUD rewrite.

--- web/src/utils/text.ts
+++ web/src/utils/text.ts
@@ -1,9 +1,13 @@
 /** Strip ANSI escape codes (CSI, OSC, charset) from terminal output. */
+// eslint-disable-next-line no-control-regex
+const CSI = /\x1b\[[0-9;]*[a-zA-Z]/g;
+// eslint-disable-next-line no-control-regex
+const OSC = /\x1b\][^\x07]*\x07/g;
+// eslint-disable-next-line no-control-regex
+const CHARSET = /\x1b\(B/g;
+
 export function stripAnsi(str: string): string {
-  return str
-    .replace(/\x1b\[[0-9;]*[a-zA-Z]/g, '')
-    .replace(/\x1b\][^\x07]*\x07/g, '')
-    .replace(/\x1b\(B/g, '');
+  return str.replace(CSI, '').replace(OSC, '').replace(CHARSET, '');
 }
 
 /** Truncate string to maxLen characters with ellipsis. */
